bc61f769b473f0af5f705aadad80b73cde0e4e94
Ashish Joshi <97600247+AJTITAN@users.noreply.github.com>
2024-08-18 09:05:44 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,7 @@ const connectDB=async()=>{
 dotenv.config()
 app.use(express.json())
 app.use("/images",express.static(path.join(__dirname,"/images")))
-app.use(cors({origin:"http://localhost:5173",credentials:true}))
+app.use(cors({origin:"https://blog-website-3wn8k0ok2-ashish-joshis-projects-cbcd5ed0.vercel.app/",credentials:true}))
 app.use(cookieParser())
 app.use("/api/auth",authRoute)
 app.use("/api/users",userRoute)
